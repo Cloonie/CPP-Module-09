035a0ea2edf40f4872397c9fef9ea13009fb54f7
root <rsdoot@DESKTOP-T5CUUII>
2023-11-01 23:12:33 +0800

    organize some functions in RPN.cpp

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -1,25 +1,9 @@
 #include "RPN.hpp"
 
-RPN::RPN()
-{
-
-}
-
-RPN::~RPN()
-{
-
-}
-
-RPN::RPN(const RPN& ref)
-{
-	*this = ref;
-}
-
-RPN& RPN::operator=(const RPN& ref)
-{
-	(void)ref;
-	return (*this);
-}
+RPN::RPN() {}
+RPN::~RPN() {}
+RPN::RPN(const RPN& ref) { *this = ref; }
+RPN& RPN::operator=(const RPN& ref) { (void)ref; return (*this); }
 
 void RPN::calculate(std::string argument)
 {
